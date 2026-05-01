34d58442c4ce71e8b20f138f2fa7ab962481b71e
kaanismet <kaanismet7@gmail.com>
2026-04-30 19:57:11 -0500

    added: more allowed origins

--- src/main/java/com/KaanIsmetOkul/Spendid/security/SecurityConfig.java
+++ src/main/java/com/KaanIsmetOkul/Spendid/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://localhost:5174", "https://spendid-react.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
